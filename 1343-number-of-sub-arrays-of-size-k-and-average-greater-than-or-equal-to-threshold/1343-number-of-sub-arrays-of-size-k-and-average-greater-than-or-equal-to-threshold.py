ebfd600dcdb25cc0d27e0974ee9ca9ed593a1009
subhan2003 <127997907+subhan2003@users.noreply.github.com>
2023-12-25 04:28:59 -0600

    Time: 462 ms (97.18%), Space: 30.7 MB (5.10%) - LeetHub

--- 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
+++ 1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold/1343-number-of-sub-arrays-of-size-k-and-average-greater-than-or-equal-to-threshold.py
@@ -5,12 +5,12 @@ class Solution:
         curr_sum = 0
         
         for R in range(len(arr)):
-            curr_sum = curr_sum + arr[R]
+            curr_sum += arr[R]
             if R - L + 1 == k:
                 if curr_sum/k >= threshold:
-                    counter = counter + 1
-                curr_sum = curr_sum - arr[L]
-                L = L + 1
+                    counter += 1
+                curr_sum -= arr[L]
+                L += 1
                
         return counter
             
